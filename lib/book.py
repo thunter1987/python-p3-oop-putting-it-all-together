b2d5d2f3571a559936f6dffc75957ad68b1ef770
Tony <thunter1987@yahoo.com>
2023-08-03 00:39:12 -0500

    partially passing tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        if page_count is not type(int):
+            print("page_count must be an integer")
+        else:
+            self.page_count = page_count
