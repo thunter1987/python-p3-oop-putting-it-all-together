48cf305ec2c6472667c78da4b8c455d15ff601bc
Tony <thunter1987@yahoo.com>
2023-08-04 12:05:00 -0500

    Passes Tests

--- lib/book.py
+++ lib/book.py
@@ -4,7 +4,18 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-        if page_count is not type(int):
-            print("page_count must be an integer")
+        
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
         else:
-            self.page_count = page_count
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
